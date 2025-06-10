a0b782bf3451249c642560b22edcfc0ef21a31e0
sssspoddub <spoddub@yahoo.com>
2025-06-10 17:57:54 +0300

    second step

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,4 +8,10 @@ def main():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument(
+        '-f', '--format',
+        metavar='FORMAT',
+        default='stylish',
+        help='set format of output'
+    )
     args = parser.parse_args()
