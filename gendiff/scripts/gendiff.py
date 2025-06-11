e9c08a8f080d4d731a54a86f737ed30b184caa2c
sssspoddub <spoddub@yahoo.com>
2025-06-11 18:34:44 +0300

    fix

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,18 @@
+import argparse
+
+from gendiff import generate_diff
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='gendiff',
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', metavar='FORMAT',
+                        default='stylish', help='set format of output')
+    args = parser.parse_args()
+
+    result = generate_diff(args.first_file, args.second_file)
+    print(result)
