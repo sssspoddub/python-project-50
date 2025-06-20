57f55412f895bffaa42e4a9deeac9f9e3cd04ead
sssspoddub <spoddub@yahoo.com>
2025-06-20 14:24:46 +0300

    fix

--- gendiff/formatters/json.py
+++
@@ -1,6 +0,0 @@
-import json
-from typing import Dict, List
-
-
-def format_json(tree: List[Dict]) -> str:
-    return json.dumps(tree, indent=4, sort_keys=True)
--- gendiff/readers.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from typing import Mapping
-
-from gendiff.parsers import parse
-
-
-def read_file(path: str | Path | Mapping) -> dict:
-    return parse(path)
